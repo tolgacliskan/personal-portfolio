1358d4890875bbca12030263b9a96e714ca5b367
Tolga Çalışkan <tolgacliskan@gmail.com>
2024-09-25 21:34:31 +0300

    Website link changed

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -35,7 +35,7 @@ export const DATA = {
   contactButtons: [
     {
       type: "Website",
-      href: "https://tolgacliskan.com?deneme",
+      href: "https://turkticaret.net",
       icon: <Icons.globe className="size-3" />,
     },
     {
