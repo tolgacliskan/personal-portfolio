6197dde54788a8d4683dcf86d9fb9596595a2b44
Tolga Çalışkan <tolgacliskan@gmail.com>
2025-04-02 11:54:05 +0300

    Yazı küçültüld

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
             <div className="flex-col flex flex-1 space-y-2">
               <BlurFadeText
                 delay={BLUR_FADE_DELAY}
-                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                className="text-2xl font-bold tracking-tighter sm:text-4xl xl:text-4xl/none"
                 yOffset={8}
                 text={`${DATA.name}`}
               />
